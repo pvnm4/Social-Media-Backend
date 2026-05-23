c4215d6f322aaa83f8fb8b78ccf337b254882264
pavan <pavanmhaisne@gmail.com>
2026-05-23 14:09:19 +0530

    "added voting system"

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr 
 from datetime import datetime
 from typing import Optional
+from pydantic.types import conint
 
 class PostBase(BaseModel):
     title: str
@@ -51,3 +52,7 @@ class Token(BaseModel):
 
 class TokenData(BaseModel):
     id: Optional[int] = None
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(le=1)

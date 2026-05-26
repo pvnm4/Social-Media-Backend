019f68e4ed511c12c993528e0f9e45a46f676bc4
pavan <pavanmhaisne@gmail.com>
2026-05-26 14:10:52 +0530

    Improved the voting logic

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,6 @@
-from pydantic import BaseModel, EmailStr 
+from pydantic import BaseModel, EmailStr, Field
 from datetime import datetime
-from typing import Optional
-from pydantic.types import conint
+from typing import Optional, Annotated
 
 class PostBase(BaseModel):
     title: str
@@ -55,4 +54,4 @@ class TokenData(BaseModel):
 
 class Vote(BaseModel):
     post_id: int
-    dir: conint(le=1)
+    dir: Annotated[int, Field(le=1)]
